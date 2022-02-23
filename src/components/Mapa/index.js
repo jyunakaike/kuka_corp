7674373aa070c4730cdd39adc6969c17bc2dc863
jyun <jyunakaike@gmail.com>
2022-02-23 19:05:44 -0400

    feat: mapa component

--- src/components/Mapa/index.js
+++ src/components/Mapa/index.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './styles.css';
+import mapa from '../../../assets/image/mapa.jpg';
+
+
 
 export const Mapa = () => {
   return (
-      <section>
-          esto es mapa
-
+      <section className='Mapa-container'>
+        <img className='Mapa-img' src={mapa} alt="mapa" />
       </section>
   )
 }
