a391171e98a8c707b14dd3b6f1f72d8680d51522
jyun <jyunakaike@gmail.com>
2022-02-24 15:10:37 -0400

    fix : introduction parallax

--- src/components/Introduction/index.js
+++ src/components/Introduction/index.js
@@ -6,23 +6,34 @@ import { Parallax, Background } from 'react-parallax';
 
 import bg from '../../../assets/image/bg.jpg'
 
+
+const image2 = "https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+
 export const Introduction = () => {
     return (
-        <section className='Introduction-container'>
-            {/* <img className='Introduction-image' src={require(`../../../assets/image/bg.jpg`)} /> */}
+        // <Parallax bgImageStyle={{height: '100%', width: '100%', minWidth:'1000px'}}  className='Introduction-container' bgImage={image2} strength={150}>
+        // </Parallax>
+        <Parallax bgImageStyle={{height: '100%', width: '100%', minWidth:'1000px'}}  className='Introduction-container' bgImage={bg} strength={150}>
+        </Parallax>
 
-            {/* parallax clavado */}
-            {/* <img className='Introduction-image' style={{backgroundImage:`url(${bg})` }} /> */}
+// , maxWidth: '75px', opacity: '.5'
 
-            <Parallax className='Introduction-image' bgImage={bg} bgImageAlt="bg" strength={-120}>
+        // </Parallax>
+        // <section className='Introduction-container'>
+        //     {/* <img className='Introduction-image' src={require(`../../../assets/image/bg.jpg`)} /> */}
 
-            </Parallax>
+        //     {/* parallax clavado */}
+        //     {/* <img className='Introduction-image' style={{backgroundImage:`url(${bg})` }} /> */}
 
-            {/* <Parallax strength={-200}>
-                <Background className='Introduction-image' >
-                    <img className='Introduction-image' src={require(`../../../assets/image/bg.jpg`)} />
-                </Background>
-            </Parallax> */}
-        </section>
+        //     {/* <Parallax className='Introduction-image' bgImage={bg} bgImageAlt="bg" strength={-120}>
+
+        //     </Parallax> */}
+
+        //     {/* <Parallax className='Introduction-image' strength={-120}>
+        //         <Background className='Introduction-image' >
+        //             <img className='Introduction-image' src={require(`../../../assets/image/bg.jpg`)} />
+        //         </Background>
+        //     </Parallax> */}
+        // </section>
     )
 }
