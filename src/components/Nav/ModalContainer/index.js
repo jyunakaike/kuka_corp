4130d95cc6c104e687fc203b206d83f75e2e3feb
jyun <jyunakaike@gmail.com>
2022-02-23 20:28:41 -0400

    feat: modal component

---
+++ src/components/Nav/ModalContainer/index.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react'
+import './styles.css'
+
+export const ModalContainer = ({ setOpenModal }) => {
+
+    useEffect(() => {
+        document.body.style.overflow = 'hidden';
+    }, [])
+
+
+    const closeModal = () => {
+        document.body.style.overflow = 'unset';
+        setOpenModal(false)
+    }
+
+
+    return (
+        <React.Fragment>
+            <div className='ModalContainer-overlay' >
+                <div className='ModalContainer-overlay-container '>
+                    <div className='ModalContainer-right'>
+                        <div className='ModalContainer-Close' onClick={closeModal} > X </div>
+                    </div>
+                    <div className='ModalContainer-element'>Aqui viene el QR</div>
+                </div>
+
+            </div>
+        </React.Fragment>
+    )
+}
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { ModalContainer } from './ModalContainer';
+
+
 import './styles.css'
 
 export const Nav = () => {
+  // for modal
+  const [openModal, setOpenModal] = useState(false);
+
+  const openModalQR = () => {
+    setOpenModal(true)
+  }
+
+
+
+
   return (
     <header >
       <h1>Kuka</h1>
@@ -10,8 +24,15 @@ export const Nav = () => {
         <li>KUKA INTRO</li>
         <li>AQS</li>
         <li>PUNTOS DE VENTA</li>
-        <li>QR</li>
+        <li onClick={openModalQR} >QR</li>
       </ul>
+
+
+
+      {
+        (openModal) && 
+        <ModalContainer  setOpenModal ={setOpenModal}/>
+      }
     </header>
   )
 }
