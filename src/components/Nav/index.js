b522c9d089ce1dbececf226580a9bd977c33dfd7
jyun <jyunakaike@gmail.com>
2022-02-28 11:35:32 -0400

    feat: header titles hover, style and build production

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,58 +3,49 @@ import React, { useState } from 'react'
 import { ModalContainer } from './ModalContainer';
 import './styles.css'
 
-import { KukakorpMenu } from './KukakorpMenu';
-import { KukaIntro} from './KukaIntro';
-
 import Logo from '../../../assets/logos/headerLogo.png'
 
 export const Nav = () => {
   // for modal
   const [openModal, setOpenModal] = useState(false);
-  const [openMenukukacorp, setOpenMenukukacorp] = useState(false);
-  const [openMenuIntro, setOpenMenuIntro] = useState(false);
 
   const openModalQR = () => {
     setOpenModal(true)
-  }
-
-  const kukacorpClick = () => {
-    setOpenMenukukacorp(!openMenukukacorp)
-  }
-
-  const kukaintroClick = () => {
-    setOpenMenuIntro(!openMenuIntro)
   }
 
   return (
     <React.Fragment>
       <header >
         <img src={Logo}></img>
-        
-        <ul>
+
+        <div className='navbar-titles'>
+
           <div className='Kukakcorp'>
-            <li onClick={kukacorpClick}>
-             <h2>KUKACORP</h2>
-            </li> 
-            {
-              (openMenukukacorp) &&
-              <KukakorpMenu />
-            }
+            <h2>KUKACORP</h2>
+            <div className='KukakorpMenu-container'>
+              <h2>MISIÓN</h2>
+
+              <h2>VISIÓN</h2>
+
+              <h2>CONTACTO</h2>
+            </div>
           </div>
 
           <div className='KukaIntro'>
-            <li onClick={kukaintroClick}> <h2> KUKA INTRO</h2></li>
-            {
-              (openMenuIntro) &&
-              <KukaIntro />
-            }
+            <div> <h2> KUKA INTRO</h2></div>
+            <div className='KukaIntro-Menu'>
+              <h2>INTRO</h2>
+              <h2>QUÉ ES?</h2>
+              <h2>INGREDIENTES</h2>
+              <h2>INFORMACIÓN NUTRICIONAL</h2>
+              <h2>BENEFICIOS</h2>
+            </div>
           </div>
-          
-          <li> <h2>FAQS</h2> </li>
-          <li> <h2>PUNTOS DE VENTA</h2> </li>
-          <li onClick={openModalQR} > <h2>WHATSAPP</h2> </li>
-        </ul>
 
+          <div> <h2>FAQS</h2> </div>
+          <div> <h2>PUNTOS DE VENTA</h2> </div>
+          <div onClick={openModalQR} className="header-whatsapp" > <h2>WHATSAPP</h2> </div>
+        </div>
       </header>
       {
         (openModal) &&
